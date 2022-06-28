9c220bef2d6f10a66ed68345d9bf84fcc86c426b
Andrew Stellwag <aestellwag@gmail.com>
2022-06-28 15:11:29 -0400

    Updated plugin version

--- src/OutboundDialingSipPlugin.js
+++ src/OutboundDialingSipPlugin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlexPlugin } from 'flex-plugin';
+import { FlexPlugin } from '@twilio/flex-plugin';
 
 // import the custom listeners
 import './listeners/CustomListeners';
